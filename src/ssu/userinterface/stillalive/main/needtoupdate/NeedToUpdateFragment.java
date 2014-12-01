29d341573397200e4be7079c86e1c87603fee31f
loki <growingdever@gmail.com>
2014-12-02 02:20:18 +0900

    버튼 클릭 유도 화면에서 애니메이션 넣고.... 이거 좀 많이 바꿔볼 생각임.

--- src/ssu/userinterface/stillalive/main/needtoupdate/NeedToUpdateFragment.java
+++ src/ssu/userinterface/stillalive/main/needtoupdate/NeedToUpdateFragment.java
@@ -11,12 +11,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class NeedToUpdateFragment extends Fragment implements OnClickListener {
+public class NeedToUpdateFragment extends Fragment implements OnClickListener, AnimationListener {
 	
 	private static final String TAG = "NeedToUpdateFragment";
-	private Button btnAlive;
+	
+	Button _buttonAlive;
+	ImageView _imageViewButtonBack;
+	
+	Animation _scaleAnimatiton;
+	
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedistanceState) {
 		return inflater.inflate(R.layout.fragment_need_to_update, container, false);
@@ -30,8 +39,17 @@ public class NeedToUpdateFragment extends Fragment implements OnClickListener {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		btnAlive = (Button) getView().findViewById(R.id.btnAlive);
-		btnAlive.setOnClickListener(this);
+		
+		_scaleAnimatiton = AnimationUtils.loadAnimation(this.getActivity(), R.anim.scale_alive_button);
+		_scaleAnimatiton.setAnimationListener(this);
+		_imageViewButtonBack = (ImageView) getView().findViewById(R.id.fragment_need_to_update_button_background);
+		_imageViewButtonBack.startAnimation(_scaleAnimatiton);
+		
+		Animation rotation = AnimationUtils.loadAnimation(this.getActivity(), R.anim.rotate_alive_button);
+		rotation.setRepeatCount(Animation.INFINITE);
+		_buttonAlive = (Button) getView().findViewById(R.id.btnAlive);
+		_buttonAlive.setOnClickListener(this);
+		_buttonAlive.startAnimation(rotation);
 	}
 
 	@Override
@@ -51,6 +69,24 @@ public class NeedToUpdateFragment extends Fragment implements OnClickListener {
 	void SetStateToMain() {
 		MainActivity parent = (MainActivity) getActivity();
 		parent.SetState(MainActivity.STATE_FRIEND_LIST);
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		// TODO Auto-generated method stub
+		_imageViewButtonBack.startAnimation(_scaleAnimatiton);
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
