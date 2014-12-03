671c830d050573320074c1ed72404628b516b250
loki <growingdever@gmail.com>
2014-12-04 02:37:32 +0900

    설정에서 summary 세팅과 on change listener 분리. 상태 메세지 변경하면 서버로 전송함.

--- src/ssu/userinterface/stillalive/main/SettingsActivity.java
+++ src/ssu/userinterface/stillalive/main/SettingsActivity.java
@@ -1,5 +1,12 @@
 package ssu.userinterface.stillalive.main;
 
+import java.util.Hashtable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -7,19 +14,32 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 import ssu.userinterface.stillalive.R;
+import ssu.userinterface.stillalive.common.Config;
+import ssu.userinterface.stillalive.common.HTTPHelper;
+import ssu.userinterface.stillalive.common.HTTPHelper.OnResponseListener;
 
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+	
+	private static final String TAG = "SettingsActivity"; 
+
+	String _accessToken;
+	
 
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
+		
+		SharedPreferences pref = getSharedPreferences("default", Context.MODE_PRIVATE);
+		_accessToken = pref.getString("accessToken", "");
+		
+		
 
 		setupSimplePreferencesScreen();
 	}
@@ -32,64 +52,75 @@ public class SettingsActivity extends PreferenceActivity {
 		getPreferenceScreen().addPreference(fakeHeader);
 		addPreferencesFromResource(R.xml.pref_notification);
 
-		bindPreferenceSummaryToValue(findPreference("state_message"));
-		bindPreferenceSummaryToValue(findPreference("notifications_ringtone"));
+		Preference prefStateMessage = findPreference("state_message");
+		Preference prefNoti = findPreference("notifications_ringtone");
+		updateSummaryByLegacyValue(prefStateMessage);
+		updateSummaryByLegacyValue(prefNoti);
+
+		prefStateMessage.setOnPreferenceChangeListener(this);
+		prefNoti.setOnPreferenceChangeListener(this);
 	}
-
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
-
-			if (preference instanceof RingtonePreference) {
-				if (TextUtils.isEmpty(stringValue)) {
-					preference.setSummary(R.string.pref_ringtone_silent);
-				} else {
-					Ringtone ringtone = RingtoneManager.getRingtone(preference.getContext(), Uri.parse(stringValue));
-
-					if (ringtone == null) {
-						preference.setSummary(null);
-					} else {
-						String name = ringtone.getTitle(preference.getContext());
-						preference.setSummary(name);
-					}
-				}
-
-			} else {
-				preference.setSummary(stringValue);
-			}
-			return true;
-		}
-	};
-
-	private static void bindPreferenceSummaryToValue(Preference preference) {
-		// Set the listener to watch for value changes.
-		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-		// Trigger the listener immediately with the preference's current value.
-		sBindPreferenceSummaryToValueListener.onPreferenceChange(
-				preference,
+	
+	void updateSummaryByLegacyValue(Preference preference) {
+		updateSummary(preference, 
 				PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
 	}
+	
+	void updateSummary(Preference preference, Object value) {
+		String stringValue = (String)value;
+		
+		if (preference.getTitleRes() == R.string.pref_title_notifications) {
+			if (TextUtils.isEmpty(stringValue)) {
+				preference.setSummary(R.string.pref_ringtone_silent);
+			} else {
+				Ringtone ringtone = RingtoneManager.getRingtone(preference.getContext(), Uri.parse(stringValue));
 
-	public static class GeneralPreferenceFragment extends PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_general);
-
-			bindPreferenceSummaryToValue(findPreference("example_text"));
+				if (ringtone == null) {
+					preference.setSummary(null);
+				} else {
+					String name = ringtone.getTitle(preference.getContext());
+					preference.setSummary(name);
+				}
+			}
+		} else {
+			preference.setSummary(stringValue);
 		}
 	}
 
-	public static class NotificationPreferenceFragment extends
-			PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_notification);
-			
-			bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
+	private void bindPreferenceSummaryToValue(Preference preference) {
+		preference.setOnPreferenceChangeListener(this);
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object value) {
+		updateSummary(preference, value);
+		
+		String stringValue = value.toString();
+		if( preference.getTitleRes() == R.string.pref_title_state_message ) {
+			Hashtable<String, Object> parameters = new Hashtable<String, Object>();
+			parameters.put("access_token", _accessToken);
+			parameters.put("message", stringValue);
+			HTTPHelper.POST(Config.HOST + "/users/state_message",
+					"application/json",
+					parameters, 
+					new OnResponseListener() {
+				
+				@Override
+				public void OnResponse(String response) {
+					try {
+						JSONObject json = new JSONObject(response);
+						if( json.getInt("result") == 1) {
+							Toast.makeText(getApplicationContext(), "Changed!", Toast.LENGTH_SHORT).show();
+						} else {
+							Toast.makeText(getApplicationContext(), "Server error...", Toast.LENGTH_SHORT).show();
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+			});
 		}
+
+		return true;
 	}
 }
