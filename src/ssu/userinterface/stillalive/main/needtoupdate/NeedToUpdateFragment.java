3a439d6b957584c8efcbad121438b286a05f1c95
potenn <menyoy@naver.com>
2014-12-05 22:21:40 +0900

    시간별로 메세지 다양화

--- src/ssu/userinterface/stillalive/main/needtoupdate/NeedToUpdateFragment.java
+++ src/ssu/userinterface/stillalive/main/needtoupdate/NeedToUpdateFragment.java
@@ -1,14 +1,12 @@
 package ssu.userinterface.stillalive.main.needtoupdate;
 
 import ssu.userinterface.stillalive.R;
-import ssu.userinterface.stillalive.common.Config;
 import ssu.userinterface.stillalive.common.TimeChecker;
 import ssu.userinterface.stillalive.main.MainActivity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +16,7 @@ import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class NeedToUpdateFragment extends Fragment implements OnClickListener, AnimationListener {
 	
@@ -26,13 +25,57 @@ public class NeedToUpdateFragment extends Fragment implements OnClickListener, A
 	SharedPreferences.Editor editor;
 	Button _buttonAlive;
 	ImageView _imageViewButtonBack;
-	
+	TextView _message;
 	Animation _scaleAnimatiton;
 	
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedistanceState) {
 		return inflater.inflate(R.layout.fragment_need_to_update, container, false);
 	}
+
+	
+	
+	/*
+	 * 
+	 * (non-Javadoc)
+	 * @see android.app.Fragment#onResume()
+	 * 
+	 * Time
+	 * 0~10분
+	 * 10~15분
+	 * 15~25분
+	 * 25~30분
+	 * 30분~
+	 */
+	@Override
+	public void onResume() {
+		super.onResume();
+	      long gap = TimeChecker.getInstance().getCurrentGapTimeFromBefore(getActivity());
+	      if(gap<=1000*600&gap>=0)
+	      {
+	         _message.setText("Hi there!");
+	      }
+	      else if(gap>=1000*600 && gap<=900*1000)
+	      {
+	         _message.setText("Hah?");
+	      }
+	      else if(gap>=900*1000 && gap<=1500*1000)
+	      {
+	         _message.setText("Anybody there?");
+	      }
+	      else if(gap>=1500*1000 && gap<=1800*1000)
+	      {
+	         _message.setText("A-yo man, are you alive?");
+	      }
+	      else
+	      {
+	         _message.setText("Please tell me your safety!");
+	      }
+		
+	}
+
+
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -49,7 +92,7 @@ public class NeedToUpdateFragment extends Fragment implements OnClickListener, A
 		_scaleAnimatiton.setAnimationListener(this);
 		_imageViewButtonBack = (ImageView) getView().findViewById(R.id.fragment_need_to_update_button_background);
 		_imageViewButtonBack.startAnimation(_scaleAnimatiton);
-		
+		_message = (TextView)getView().findViewById(R.id.message);
 		Animation rotation = AnimationUtils.loadAnimation(this.getActivity(), R.anim.rotate_alive_button);
 		rotation.setRepeatCount(Animation.INFINITE);
 		_buttonAlive = (Button) getView().findViewById(R.id.btnAlive);
