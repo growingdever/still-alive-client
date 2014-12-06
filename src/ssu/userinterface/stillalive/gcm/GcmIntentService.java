3ec80f023bc2d2881b8b5445628b8fa8f848dfb9
loki <growingdever@gmail.com>
2014-12-06 16:53:58 +0900

    설정에 따라 노티피케이션....

--- src/ssu/userinterface/stillalive/gcm/GcmIntentService.java
+++ src/ssu/userinterface/stillalive/gcm/GcmIntentService.java
@@ -7,11 +7,13 @@ import ssu.userinterface.stillalive.main.inbox.InboxActivity;
 
 import android.app.Activity;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
@@ -26,75 +28,97 @@ public class GcmIntentService extends IntentService {
 
     public GcmIntentService() {
         super(TAG);
-
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
         Bundle extras = intent.getExtras();
-
+        
         if (!extras.isEmpty()) {
             int messageType = Integer.parseInt(extras.getString("type"));
             if (messageType == Config.PUSH_MESSAGE_TYPE_POKE) {
-                SendUpdatingRequestNotification(extras.getString("senderUserID"));
+           		SendUpdatingRequestNotification(extras.getString("senderUserID"));
 
                 SharedPreferences pref = getSharedPreferences("default", MODE_PRIVATE);
                 SharedPreferences.Editor editor = pref.edit();
                 editor.putBoolean("shouldUpdate", true);
                 editor.commit();
             } else if (messageType == Config.PUSH_MESSAGE_TYPE_POKE) {
-                SendFriendRequestNotification(extras.getString("senderUserID"));
+           		SendFriendRequestNotification(extras.getString("senderUserID"));
             }
-
         }
 
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 
     void SendUpdatingRequestNotification(String senderID) {
-        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
+    	boolean useNotification = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("notifications_setting", false);
+    	if( !useNotification ) {
+    		return;
+    	}
+    	
+        Uri ringtoneURI = Uri.parse(PreferenceManager.getDefaultSharedPreferences(this).getString("notifications_ringtone", ""));
+        boolean useVibrate = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("notifications_vibrate", false);
         String msg = senderID + " want to know your safety";
+        
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         PendingIntent contentIntent = PendingIntent.getActivity(this,
                 0,
                 new Intent(this, MainActivity.class),
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder mBuilder =
+        NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle("StillAlive")
                         .setTicker("StillAlive")
                         .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                         .setContentText(msg)
-                        .setAutoCancel(true);
-
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+                        .setAutoCancel(true)
+                        .setContentIntent(contentIntent);
+        if( useVibrate ) {
+        	builder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        }
+        
+        Notification notification = builder.build();
+        notification.sound = ringtoneURI;
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
     void SendFriendRequestNotification(String senderID) {
-        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
+    	boolean useNotification = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("notifications_setting", false);
+    	if( !useNotification ) {
+    		return;
+    	}
+    	
+    	Uri ringtoneURI = Uri.parse(PreferenceManager.getDefaultSharedPreferences(this).getString("notifications_ringtone", ""));
+    	boolean useVibrate = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("notifications_vibrate", false);
         String msg = senderID + " like to be your friend";
+        
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         PendingIntent contentIntent = PendingIntent.getActivity(this,
                 0,
                 new Intent(this, InboxActivity.class),
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder mBuilder =
+        NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle("StillAlive")
                         .setTicker("StillAlive")
                         .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                         .setContentText(msg)
-                        .setAutoCancel(true);
-
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+                        .setAutoCancel(true)
+                        .setContentIntent(contentIntent);
+        if( useVibrate ) {
+        	builder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        }
+        
+        Notification notification = builder.build();
+        notification.sound = ringtoneURI;
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
 }
